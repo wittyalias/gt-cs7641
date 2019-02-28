f1f73746907bcad9d84afa161a0bede4b681614e
kmoney <larsonk@live.com.au>
2019-02-28 13:35:11 -0800

    sams commit

--- Randomized Optimization/code/genetic.R
+++ Randomized Optimization/code/genetic.R
@@ -4,82 +4,139 @@
 # Uncomment if you need to install
 #install.packages("GA")
 #install.packages("neuralnet")
-#install.packages("gmodels")
-require("GA")
-require("neuralnet")
-require("gmodels")
+#install.packages("gnew_models")
+library("dplyr")
+library("GA")
+library("neuralnet")
+library("gmodels")
+library("caret")
+
+
+# tryCatch(
+#     setwd(dirname(parent.frame(2)$ofile)),
+#     error = function(e) {setwd("code")}
+# )
+
+#load the data
+abalone_test_set <- readRDS(file.path("..", "input",  "abalone_test_set.rds"))
+abalone_train_set <- readRDS(file.path("..", "input",  "abalone_train_set.rds"))
+
+model <- readRDS(file = file.path("..", "output", "nn-base.rds"))
+
+new_model <- model
+
+train_actual <- abalone_train_set %>% 
+    select(young, middling, old) %>% 
+    max.col %>% 
+    factor
+
+test_actual <- abalone_test_set %>% 
+    select(young, middling, old) %>% 
+    max.col %>% 
+    factor
 
 # Settings
-iterations <- 100
+iterations <-50
 
-# Functions
-normalize <- function(x) {
-  return ((x - min(x, na.rm=TRUE)) / (max(x, na.rm=TRUE) - min(x, na.rm=TRUE)))
-}
-
-maxfactor <- function(x) {
-  return(which(x == max(x)))
-}
-
-# This place revealed the mystery of the nearualnet formula https://www.r-bloggers.com/fitting-a-neural-network-in-r-neuralnet-package/
-f <- as.formula(paste("young + middling + old ~", paste(names(abalone_train_set)[!names(abalone_train_set) %in% c("young" ,"middling", "old")], collapse = "+"))) 
-
-                        
-# # Create our model, training and testing sets
-# initial_model_set <- pimadata_use[1:(nrow(pimadata)-100), ]
-# 
-# training_set <- pimadata_use[1:(nrow(pimadata)-100),1:8]
-# training_set$Diagnosis <- as.factor(
-#   as.integer(xor(1,pimadata$Diagnosis[1:(nrow(pimadata)-100)])))
-# levels(training_set$Diagnosis) <- c("Diabetic", "Normal")
-# 
-# testing_set <- pimadata_use[(nrow(pimadata)-99):nrow(pimadata),1:8]
-# testing_set$Diagnosis <- as.factor(
-#   as.integer(xor(1,pimadata$Diagnosis[(nrow(pimadata)-99):nrow(pimadata)])))
-# levels(testing_set$Diagnosis) <- c("Diabetic", "Normal")
-
-# Create our neural network model
-model <- neuralnet(formula = f,
-                   data = abalone_test_set,
-                   hidden = 0)
+# maxfactor <- function(x) {
+#   return(which(x == max(x)))
+# }
 
 # Neural Network as Fitness Function
 
-fit_func <- function(string=c()) {
-      model$weights[[1]][[1]][,1] <- string[1:9]
-      model$weights[[1]][[1]][,2] <- string[10:18]
-      prediction <- compute(model, training_set[1:8])$net.result
-      classification <- apply(prediction, c(1), maxfactor)
-      prediction <- c('Diabetic', 'Normal')[classification]
-      result <- table(prediction,training_set$Diagnosis)
-      return(sum(prediction == training_set$Diagnosis)/sum(result))
+# As defined in ?GA::ga :
+# the fitness function, any allowable R function which takes as input an individual string representing a potential solution, and returns a numerical value describing its “fitness”.
+
+# An attempt to edit the previous fitness function to accomodate a new_model with 10 hidden nodes and 10 inputs (plust a bias)
+fit_func_edit <- function(string=c()) {
+    
+    # Weights for the hidden layer    
+    for (j in 1:10){
+        new_model$weights[[1]][[1]][,j] <- string[(1 + 11*(j-1)):(11*j)]
+    }
+    # Weights for the output layer
+    for(j in 1:3){
+        new_model$weights[[1]][[2]][,j] <- string[((111 + 11*(j-1))):(110 + (11*j))]
+    }
+
+    prediction <- compute(new_model, 
+                          abalone_train_set[!names(abalone_train_set) %in% c("young" ,"middling", "old")])$net.result %>% 
+        max.col %>% 
+        factor
+
+    return(confusionMatrix(prediction, train_actual)$overall["Accuracy"])
 }
+
+# for (j in 1:3){
+#     cat(paste("[(1 + 11*(j-1)): ", (111 + 11*(j-1)),"(11*j): ", 110+(11*j), "\n"))
+# }
+
+
+# fit_func <- function(string=c()) {
+#       new_model$weights[[1]][[1]][,1] <- string[1:9]
+#       new_model$weights[[1]][[1]][,2] <- string[10:18]
+#       prediction <- compute(new_model, training_set[1:8])$net.result
+#       classification <- apply(prediction, c(1), maxfactor)
+#       prediction <- c('Diabetic', 'Normal')[classification]
+#       result <- table(prediction,training_set$Diagnosis)
+#       return(sum(prediction == training_set$Diagnosis)/sum(result))
+# }
 
 ptm <- proc.time()
 
+# Using the abalone dataset with a neural network with 10 hidden nodes
+# The abalone dataset has 10 features, meaning that there are 11 weights (inc. bias)
+# for each of the nodes -> 110 weights. Plus 11 weights for each of the three output
+# layer nodes -> 33 weights. So the genetic algorithm is going to be adjusting 
+# 143 values, that are the weights of the new_model to optimize it. 
+
+monitor_func <- function(obj) 
+{ 
+    contour(x1, x2, f, drawlabels = FALSE, col = grey(0.5))
+    title(paste("iteration =", obj@iter), font.main = 1)
+    points(obj@population, pch = 20, col = 2)
+    Sys.sleep(0.2)
+}
+
 ga_result <- ga(type = "real-valued",
-                fitness = fit_func,
-                min = rep(-5,18),
-                max = rep(5,18),
+                fitness = fit_func_edit,
+                lower = rep(-3,143),
+                upper = rep(3,143),
                 popSize = 1000,
-                maxiter = iterations)
+                maxiter = iterations,
+                monitor = monitor_func)
 
 cat("            Time:",(proc.time() - ptm),"\n")
 cat(" Optimal Weights:",ga_result@solution[1,],"\n")
 
-model$weights[[1]][[1]][,1] <- as.vector(ga_result@solution[1,1:9])
-model$weights[[1]][[1]][,2] <- as.vector(ga_result@solution[1,10:18])
-prediction <- compute(model, testing_set[1:8])$net.result
-classification <- apply(prediction, c(1), maxfactor)
-prediction <- c('Diabetic', 'Normal')[classification]
-result <- table(prediction,testing_set$Diagnosis)
+for (j in 1:10){
+    new_model$weights[[1]][[1]][,j] <- ga_result@solution[1,][(1 + 11*(j-1)):(11*j)]
+}
+# Weights for the output layer
+for(j in 1:3){
+    new_model$weights[[1]][[2]][,j] <- ga_result@solution[1,][((111 + 11*(j-1))):(110 + (11*j))]
+}
 
-cat("        Accuracy:",sum(prediction == testing_set$Diagnosis)/sum(result),"\n")
+prediction <- compute(new_model, abalone_train_set[!names(abalone_train_set) %in% c("young" ,"middling", "old")])$net.result %>% 
+    max.col %>% 
+    factor
 
-CrossTable(x = testing_set$Diagnosis, 
-           y = prediction,
-           prop.r = FALSE,
-           prop.c = FALSE,
-           prop.t = FALSE,
-           prop.chisq = FALSE,
-           dnn = c("Actual", "Prediction"))  
+confusionMatrix(prediction, train_actual)
+
+test_pred <- compute(new_model, abalone_test_set[!names(abalone_test_set) %in% c("young" ,"middling", "old")])$net.result %>% 
+    max.col %>% 
+    factor
+
+confusionMatrix(test_pred, test_actual)
+
+
+
+# cat("        Accuracy:",sum(prediction == testing_set$Diagnosis)/sum(result),"\n")
+# 
+# CrossTable(x = testing_set$Diagnosis, 
+#            y = prediction,
+#            prop.r = FALSE,
+#            prop.c = FALSE,
+#            prop.t = FALSE,
+#            prop.chisq = FALSE,
+#            dnn = c("Actual", "Prediction"))  
---
+++ Randomized Optimization/code/nerualnet-base.R
@@ -0,0 +1,49 @@
+
+library("neuralnet")
+library("dplyr")
+library("caret")
+
+#load the data
+abalone_test_set <- readRDS(file.path("..", "input",  "abalone_test_set.rds"))
+abalone_train_set <- readRDS(file.path("..", "input",  "abalone_train_set.rds"))
+
+# This place revealed the mystery of the nearualnet formula https://www.r-bloggers.com/fitting-a-neural-network-in-r-neuralnet-package/
+f <- as.formula(paste("young + middling + old ~", paste(names(abalone_train_set)[!names(abalone_train_set) %in% c("young" ,"middling", "old")], collapse = "+"))) 
+
+train_actual <- abalone_train_set %>% 
+    select(young, middling, old) %>% 
+    max.col %>% 
+        factor
+
+test_actual <- abalone_test_set %>% 
+        select(young, middling, old) %>% 
+        max.col %>% 
+        factor
+
+
+# Create our neural network model
+model <- neuralnet(formula = f,
+                 data = abalone_train_set,
+                 hidden = 10,
+                 algorithm = "backprop",
+                 threshold = 0.5,
+                 learningrate = 0.0001)
+
+abalone_map <- data.frame(age = c("young" ,"middling", "old"), model_result = 1:3)
+
+nn_test_results <- neuralnet::compute(model, abalone_test_set[!names(abalone_train_set) %in% c("young" ,"middling", "old")])$net.result %>% 
+        max.col %>% 
+        factor()
+
+model_results <- model$net.result[[1]] %>% 
+    max.col %>% 
+        factor()
+
+train_acc <- confusionMatrix(model_results, train_actual)$overall["Accuracy"]
+test_acc <- confusionMatrix(nn_test_results, test_actual)$overall["Accuracy"]
+acc_df <- data.frame(Error = c("train", "test"), nn_base = c(train_acc, test_acc))
+
+
+saveRDS(model, file.path("..", "output", "nn-base.rds"))
+saveRDS(acc_df, file.path("..", "output", "acc_df.rds")
+
